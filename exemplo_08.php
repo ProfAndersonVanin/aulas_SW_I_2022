5e5f7adbeed93577a9e049b0bbea607ae88d34d0
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2022-06-02 11:03:53 -0300

    Delete exemplo_08.php

--- exemplo_08.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo "Uma $fruta $cor"; // Aqui é gerado um aviso!
-    // Mas o programa continua rodando! 
-    include 'include_08.php'; 
-    echo "Uma $fruta $cor"; // Uma maçã verde
-?>
-
-
-
