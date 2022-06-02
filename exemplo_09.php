e51a18a63b6a3b0bd786f27d6ae87ca26f4e0f05
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2022-06-02 11:04:01 -0300

    Delete exemplo_09.php

--- exemplo_09.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo "Uma $fruta $cor"; // Aqui é gerado um aviso!
-    // Mas o programa continua rodando! 
-    require 'require_09.php'; 
-    echo "Uma $fruta $cor"; // Uma maçã verde
-?>
-
-
-
